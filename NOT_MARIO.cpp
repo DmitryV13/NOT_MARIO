4a2e8edc39afbfaf4e3a494c0e1341d315c52501
maxim boev <maxim-boev@mail.ru>
2023-09-28 13:43:31 +0300

    карта

--- NOT_MARIO.cpp
+++ NOT_MARIO.cpp
@@ -1,10 +1,32 @@
 #include <SFML/Graphics.hpp>
 
+const int H = 10;
+const int W = 100;
+const int size_texture = 64;
+
+std::string TileMap[H] = {
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    "A                                                                                                 A",
+    "A                                                                                                 A",
+    "A                                                                                                 A",
+    "A         BB                                                              BB                      A",
+    "A        BBBBBB    BB                                 BBBBBB            BBBBBB      BB            A",
+    "A      BBBBBBBBBBBBBBBB              BBBBB           BBBBBBBBB  B  BB  BBBBBBBBBB BBBBBB          A",
+    "ABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBA",
+    "ABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBA",
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+};
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1500, 640), "SFML works!");
+    sf::Texture t1;
+
+    if (!t1.loadFromFile("D:\\видео\\grass_path_side.png")){
+        return -1;
+    }
+
+    sf::Sprite map(t1);
 
     while (window.isOpen())
     {
@@ -14,12 +36,20 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
-        window.draw(shape);
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                if (TileMap[i][j] == 'A' || TileMap[i][j] == 'B') {
+                    map.setTextureRect(sf::IntRect(0, 0, size_texture, size_texture));
+                }
+                else{
+                    continue;
+                }
+                map.setPosition(j * size_texture, i * size_texture);
+                window.draw(map);
+            }
+        }
         window.display();
     }
-
     return 0;
 }
-// df,ldlfd
