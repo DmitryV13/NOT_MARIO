567ee76bf21970e0d1b63057f7473141f6e48512
Dmitriy Voicu <voiku173dmitry@gmail.com>
2023-10-05 12:57:59 +0300

    part of animation

--- Player.cpp
+++ Player.cpp
@@ -1,102 +1,123 @@
-#include "Player.h"
-class Player
-{
-    void moveRight() {
-        if (spritePosition.x + ball_S.getLocalBounds().width + ballSpeed >= windowRight) {
-            if (spritePosition.x + ball_S.getLocalBounds().width + ballSpeed >= absoluteRight) {
-                windowLeft = absoluteRight - screenWidth;
-                windowRight = absoluteRight;
-                spritePosition.x = absoluteRight - ball_S.getLocalBounds().width;
-                ball_S.setPosition(spritePosition);
-                view.setCenter(windowRight - (screenWidth / 2), windowBottom - (screenHeight / 2));
-                window.setView(view);
-            }
-            else {
-                float offset = ballSpeed - (windowRight - (spritePosition.x + ball_S.getLocalBounds().width));
-                view.move(offset, 0);
-                window.setView(view);
-
-                windowLeft += offset;
-                windowRight += offset;
-
-                ball_S.move(ballSpeed, 0);
-            }
-        }
-        else {
-            ball_S.move(ballSpeed, 0);
-        }
-    }
-    void moveLeft() {
-        if (spritePosition.x - ballSpeed <= windowLeft) {
-            if (spritePosition.x - ballSpeed <= absoluteLeft) {
-                windowLeft = absoluteLeft;
-                windowRight = absoluteLeft + screenWidth;
-                spritePosition.x = absoluteLeft;
-                ball_S.setPosition(spritePosition);
-                view.setCenter(windowRight - (screenWidth / 2), windowBottom - (screenHeight / 2));
-                window.setView(view);
-            }
-            else {
-                float offset = ballSpeed - (spritePosition.x - windowLeft);
-                view.move(-offset, 0);
-                window.setView(view);
-                windowLeft -= offset;
-                windowRight -= offset;
-
-                ball_S.move(-ballSpeed, 0);
-            }
-        }
-        else {
-            ball_S.move(-ballSpeed, 0);
-        }
-    }
-    void moveUp() {
-        if (spritePosition.y - ballSpeed <= windowTop) {
-            if (spritePosition.y - ballSpeed <= absoluteTop) {
-                windowBottom = absoluteTop + screenHeight;
-                windowTop = absoluteTop;
-                spritePosition.y = absoluteTop;
-                ball_S.setPosition(spritePosition);
-                view.setCenter(windowRight - (screenWidth / 2), windowBottom - (screenHeight / 2));
-                window.setView(view);
-            }
-            else {
-                float offset = ballSpeed - (spritePosition.y - windowTop);
-                view.move(0, -offset);
-                window.setView(view);
-                windowTop -= offset;
-                windowBottom -= offset;
-
-                ball_S.move(0, -ballSpeed);
-            }
-        }
-        else {
-            ball_S.move(0, -ballSpeed);
-        }
-    }
-    void moveDown() {
-        if (spritePosition.y + ball_S.getLocalBounds().height + ballSpeed >= windowBottom) {
-            if (spritePosition.y + ball_S.getLocalBounds().height + ballSpeed >= absoluteBottom) {
-                windowBottom = absoluteBottom;
-                windowTop = absoluteBottom - screenHeight;
-                spritePosition.y = absoluteBottom - ball_S.getLocalBounds().height;
-                ball_S.setPosition(spritePosition);
-                view.setCenter(windowRight - (screenWidth / 2), windowBottom - (screenHeight / 2));
-                window.setView(view);
-            }
-            else {
-                float offset = ballSpeed - (windowBottom - (spritePosition.y + ball_S.getLocalBounds().height));
-                view.move(0, offset);
-                window.setView(view);
-
-                windowTop += offset;
-                windowBottom += offset;
-
-                ball_S.move(0, ballSpeed);
-            }
-        }
-        else {
-            ball_S.move(0, ballSpeed);
-        }
-    }
-};
+//#include "Player.h"
+//
+//
+//Player::Player(Texture hero_T, const float mapW, const float mapH, const float size_texture, const float sH, const float sW) 
+//    : absoluteRight(mapW * size_texture), 
+//    absoluteBottom(mapH * size_texture), 
+//    absoluteTop(0.0f), 
+//    absoluteLeft(0.0f), 
+//    windowLeft(0.0f), 
+//    windowTop(0.0f), 
+//    windowRight(sW), 
+//    windowBottom(sH),
+//    screenHeight(sH), 
+//    screenWidth(sW){
+//    player_S.setTexture(hero_T);
+//    player_S.setPosition(0, 0);
+//}
+//
+//void Player::moveRight(RenderWindow& window, View& view) {
+//    Vector2f spritePosition = player_S.getPosition();
+//    if (spritePosition.x + player_S.getLocalBounds().width + playerSpeed >= windowRight) {
+//        if (spritePosition.x + player_S.getLocalBounds().width + playerSpeed >= absoluteRight) {
+//            windowLeft = absoluteRight - screenWidth;
+//            windowRight = absoluteRight;
+//            spritePosition.x = absoluteRight - player_S.getLocalBounds().width;
+//            player_S.setPosition(spritePosition);
+//            view.setCenter(windowRight - (screenWidth / 2), windowBottom - (screenHeight / 2));
+//            window.setView(view);
+//        }
+//        else {
+//            float offset = playerSpeed - (windowRight - (spritePosition.x + player_S.getLocalBounds().width));
+//            view.move(offset, 0);
+//            window.setView(view);
+//
+//            windowLeft += offset;
+//            windowRight += offset;
+//
+//            player_S.move(playerSpeed, 0);
+//        }
+//    }
+//    else {
+//        player_S.move(playerSpeed, 0);
+//    }
+//}
+//void Player::moveLeft(RenderWindow& window, View& view) {
+//    Vector2f spritePosition = player_S.getPosition();
+//    if (spritePosition.x - playerSpeed <= windowLeft) {
+//        if (spritePosition.x - playerSpeed <= absoluteLeft) {
+//            windowLeft = absoluteLeft;
+//            windowRight = absoluteLeft + screenWidth;
+//            spritePosition.x = absoluteLeft;
+//            player_S.setPosition(spritePosition);
+//            view.setCenter(windowRight - (screenWidth / 2), windowBottom - (screenHeight / 2));
+//            window.setView(view);
+//        }
+//        else {
+//            float offset = playerSpeed - (spritePosition.x - windowLeft);
+//            view.move(-offset, 0);
+//            window.setView(view);
+//            windowLeft -= offset;
+//            windowRight -= offset;
+//
+//            player_S.move(-playerSpeed, 0);
+//        }
+//    }
+//    else {
+//        player_S.move(-playerSpeed, 0);
+//    }
+//}
+//
+//void Player::moveUp(RenderWindow& window, View& view) {
+//    Vector2f spritePosition = player_S.getPosition();
+//    if (spritePosition.y - playerSpeed <= windowTop) {
+//        if (spritePosition.y - playerSpeed <= absoluteTop) {
+//            windowBottom = absoluteTop + screenHeight;
+//            windowTop = absoluteTop;
+//            spritePosition.y = absoluteTop;
+//            player_S.setPosition(spritePosition);
+//            view.setCenter(windowRight - (screenWidth / 2), windowBottom - (screenHeight / 2));
+//            window.setView(view);
+//        }
+//        else {
+//            float offset = playerSpeed - (spritePosition.y - windowTop);
+//            view.move(0, -offset);
+//            window.setView(view);
+//            windowTop -= offset;
+//            windowBottom -= offset;
+//
+//            player_S.move(0, -playerSpeed);
+//        }
+//    }
+//    else {
+//        player_S.move(0, -playerSpeed);
+//    }
+//}
+//
+//void Player::moveDown(RenderWindow& window, View& view) {
+//    Vector2f spritePosition = player_S.getPosition();
+//    if (spritePosition.y + player_S.getLocalBounds().height + playerSpeed >= windowBottom) {
+//        if (spritePosition.y + player_S.getLocalBounds().height + playerSpeed >= absoluteBottom) {
+//            windowBottom = absoluteBottom;
+//            windowTop = absoluteBottom - screenHeight;
+//            spritePosition.y = absoluteBottom - player_S.getLocalBounds().height;
+//            player_S.setPosition(spritePosition);
+//            view.setCenter(windowRight - (screenWidth / 2), windowBottom - (screenHeight / 2));
+//            window.setView(view);
+//        }
+//        else {
+//            float offset = playerSpeed - (windowBottom - (spritePosition.y + player_S.getLocalBounds().height));
+//            view.move(0, offset);
+//            window.setView(view);
+//
+//            windowTop += offset;
+//            windowBottom += offset;
+//
+//            player_S.move(0, playerSpeed);
+//        }
+//    }
+//    else {
+//        player_S.move(0, playerSpeed);
+//    }
+//}
+//
