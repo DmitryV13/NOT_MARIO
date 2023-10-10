f19ed5ad4a58eef055445014420fa6e04ee9a1be
Dmitriy Voicu <voiku173dmitry@gmail.com>
2023-10-10 23:20:49 +0300

    full updated movement system done

--- Tile.h
+++ Tile.h
@@ -1,24 +1,25 @@
-#pragma once
-class Tile
-{
-private:
-	sf::Texture tile_T;
-	sf::Sprite tile_S;
-	sf::IntRect currentFrame;
-	int interaction;
-public:
-	Tile(std::string path_T, sf::IntRect textureRect, int interaction);
-	
-	void initTexture(std::string path_T);
-	void initSprite();
-
-	// Accessors
-	const sf::Vector2f getPosition() const;
-
-	// Modifiers
-	void setPosition(const float x, const float y);
-	
-	void update();
-	void render(sf::RenderTarget& target);
-};
-
+//#pragma once
+//class Tile
+//{
+//private:
+//	sf::Texture tile_T;
+//	sf::Sprite tile_S;
+//	sf::IntRect currentFrame;
+//	int interaction;
+//public:
+//	Tile(std::string path_T, sf::IntRect textureRect, int interaction);
+//	Tile();
+//	
+//	void initTexture(std::string path_T);
+//	void initSprite();
+//
+//	// Accessors
+//	const sf::Vector2f getPosition() const;
+//
+//	// Modifiers
+//	void setPosition(const float x, const float y);
+//	
+//	void update();
+//	void render(sf::RenderTarget& target);
+//};
+//
